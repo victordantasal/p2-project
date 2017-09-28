015c0bcc968646397c7e25fde34a621c7b716541
victordantasal <victor_santos9@hotmail.com>
2017-09-28 08:20:37 -0300

    Hash Table ADT para implementar

--- huffman/DataStructs/hash_table.c
+++ huffman/DataStructs/hash_table.c
@@ -6,7 +6,8 @@ typedef struct _element Element;
 struct _hash_table
 {
 	Element *table;
-	int size;
+	int maxsize;
+	int filled;
 };
 
 struct _element
@@ -15,3 +16,19 @@ struct _element
 	Element *next;
 };
 
+void* HashTable_getValue(Hash_Table *ht, int pos);
+Element* HashTable_getTable(Hash_Table *ht, int pos);
+int HashTable_getSize(Hash_Table *ht);
+int HashTable_getFilled(Hash_Table *ht);
+
+void* Element_getValue(Element *e);
+Element* Element_getNext(Element *e);
+
+bool HashTable_setValue(Hash_Table *ht, int pos, void *value);
+bool HashTable_setTable(Hash_Table *ht, int pos, Element *new_element);
+bool HashTable_setSize(Hash_Table *ht, int new_size);
+bool HashTable_setFilled(Hash_Table *ht, int new_filled);
+
+Hash_Table* HashTable_create(int size);
+bool HashTable_destruct(Hash_Table *ht);
+bool HashTable_add(Hash_Table *ht, void *value, int hash);
