84db587c3e9d7005a022661d7fb5888eaa466002
Bekinho <Bekinho@JACQUES-PC>
2017-09-28 20:07:03 -0300

    Todas as funções da binary_tree.c implementadas.

--- huffman/DataStructs/binary_tree.c
+++ huffman/DataStructs/binary_tree.c
@@ -9,41 +9,71 @@ struct _binary_tree
 
 void* BinaryTree_getValue(Binary_Tree *bt)
 {
-	//TODO method getValue
+	if(isNull(bt))
+		return ERROR;
+	return bt->value;
 }
 Binary_Tree* BinaryTree_getLeft(Binary_Tree *bt)
 {
-	//TODO method getLeft
+	if(isNull(bt))
+		return NULL;
+	return bt->left;
 }
 Binary_Tree* BinaryTree_getRight(Binary_Tree *bt)
 {
-	//TODO method getRight
+	if(isNull(bt))
+		return NULL;
+	return bt->right;
 }
 
 bool BinaryTree_setValue(Binary_Tree *bt, void* value)
 {
-	//TODO method setValue
+	if(isNull(bt))
+		return false;
+	bt->value = value;
+	return true;
 }
 bool BinaryTree_setLeft(Binary_Tree *bt, Binary_Tree *new_left)
 {
-	//TODO method setLeft
+	if(isNull(bt))
+		return false;
+	bt->left = new_left;
+	return true;
 }
 bool BinaryTree_setRight(Binary_Tree *bt, Binary_Tree *new_right)
 {
-	//TODO method setRight
+	if(isNull(bt))
+		return false;
+	bt->right = new_right;
+	return true;
 }
 
 Binary_Tree* BinaryTree_create()
 {
-	//TODO method create a binary tree
+	return NULL;
 }
 Binary_Tree* BinaryTree_new(void *value, Binary_Tree *left, Binary_Tree *right)
 {
-	//TODO method create a binary's tree new node
+	Binary_Tree *new_bt = (Binary_Tree*) malloc(sizeof(Binary_Tree));
+	if(Binary_Tree_setValue(new_bt,value) && Binary_Tree_setLeft(new_bt,left) && Binary_Tree_setRight(new_bt,right))
+		return new_bt;
+	DEBUG printf("ERROR, IT'S NOT POSSIBLE ALOCATE MEMORY");
+	return NULL;
+
+
 }
-int BinaryTree_destruct(Binary_Tree *bt)
+bool BinaryTree_destruct(Binary_Tree *bt)
 {
-	//TODO method destruct a binary tree
+	if(!isNull(bt))
+	{
+		BinaryTree_destruct(bt->left);
+		BinaryTree_destruct(bt->right);
+		free(bt);
+		bt = NULL;
+	}
+	else if(isNull(bt))
+		return true;
+	return false;
 }
 
 
