a60815cc673f91352bf3f583d104e98a0610988f
Lael <lael_lsr@hotmail.com>
2017-10-16 18:12:56 -0300

    Opa, mudei algo no compress_util.c, GetHuffmanTree

--- huffman/Util/compress_util.c
+++ huffman/Util/compress_util.c
@@ -15,7 +15,7 @@ int* get_frequency(FILE *file_reader)
 		rewind(file_reader);
 
 		//iniciando contagem das frequencias no arquivo
-		//até que se leia EOF
+		//atï¿½ que se leia EOF
 		do{
 			aux = fgetc(file_reader);
 
@@ -29,12 +29,46 @@ int* get_frequency(FILE *file_reader)
 
 Binary_Tree* get_huffmanTree(int *frequency)
 {
-	//TODO method get huffmanTree
+	Binary_Tree *bt;
+	Priority_Queue *pq = PriorityQueue_create();
+	Byte *value;
+	int i, sum;
+
+	// Se a frequencia existir entao cria-se uma arvore (com byte) e coloca ela numa fila de prioridade (na posicao 'i' do array)
+	for(i=0; i<BSIZE; i++)
+	{
+		if(frequency[i] > 0)
+		{
+			value = (Byte*)malloc(sizeof(Byte));
+			*value = i;
+			bt = BinaryTree_new(value, NULL, NULL);
+			PriorityQueue_enqueue(pq, bt, frequency[i]);
+		}
+	}
+	// Dando merge ate que a fila tenha tamanho 1
+	Binary_Tree *bt_aux_right, *bt_aux_left;
+	while(PriorityQueue_getSize(pq) > 1)
+	{
+		sum = 0;
+		sum += Node_getPriority(PriorityQueue_getHead(pq));
+		bt_aux_right = (Binary_Tree*)PriorityQueue_dequeue(pq);
+		sum += Node_getPriority(PriorityQueue_getHead(pq));
+		bt_aux_left = (Binary_Tree*)PriorityQueue_dequeue(pq);
+		value = (Byte*)malloc(sizeof(Byte));
+		*value = '*';
+		bt = BinaryTree_new(value, bt_aux_left, bt_aux_right);
+
+		PriorityQueue_enqueue(pq, bt, sum);
+	}
+
+	return (Binary_Tree*)PriorityQueue_dequeue(pq);
+
+
 }
 
 void go_through_tree(Binary_Tree *huffman_tree,Hash_Table *ht,char *new_code)
 {
-	char aux_left[9], aux_right[9];//são strings temporárias que ficarão nos auxiliando a salvar os caminhos durante a recursão
+	char aux_left[9], aux_right[9];//sï¿½o strings temporï¿½rias que ficarï¿½o nos auxiliando a salvar os caminhos durante a recursï¿½o
 
 	if(isNull(huffman_tree->left) && isNull(huffman_tree->right))
 	{
@@ -56,7 +90,7 @@ void go_through_tree(Binary_Tree *huffman_tree,Hash_Table *ht,char *new_code)
 Hash_Table* get_dictionary(Binary_Tree *huffman_tree)
 {
 	Hash_Table *ht = HashTable_create(BSIZE);
-	char new_code[8];//essa string serve para salvar o novo código de cada caractere
+	char new_code[8];//essa string serve para salvar o novo cï¿½digo de cada caractere
 	strcpy(new_code,"");//deixando a string vazia
 
 	go_through_tree(huffman_tree,ht,new_code);
