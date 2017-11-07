91271af9c22bc62150e49e5a2a9471308c41c0e7
Jacques-Wolbeck <Jacques-Wolbeck@JACQUES-PC>
2017-11-06 23:43:46 -0300

    Fiz a função string_huffmanTree no decompress_util.c(ideia inicial)

--- huffman/Util/decompress_util.c
+++ huffman/Util/decompress_util.c
@@ -5,10 +5,31 @@ bool readHeader(int *trash, char *huffmanTree_str)
 	return false;
 }
 
-Binary_Tree* stringTo_huffmanTree(char *str)
+Binary_Tree* stringTo_huffmanTree(char *str)//por enquanto só conseguir fazer usando variável global (idx),obs: não declarei em nenhum lugar o idx,por isso está dando erro
 {
-	//TODO Leonardo converter string numa arvore binaria e retornar (RECURSIVAMENTE)
-	return NULL;
+	if(str[idx] != '*')
+	{
+		if(str[idx] == '\\')
+		{
+			idx++;
+			Binary_Tree *new_bt = BinaryTree_new(str[idx],NULL,NULL);
+			return new_bt;
+		}
+		else
+		{
+			Binary_Tree *new_bt = BinaryTree_new(str[idx],NULL,NULL);
+			return new_bt;
+		}
+	}
+	else
+	{
+		Binary_Tree *new_bt = BinaryTree_new(str[idx],NULL,NULL);
+
+		idx++;
+		new_bt->left = stringTo_huffmanTree(str);
+		idx++;
+		new_bt->right = stringTo_huffmanTree(str);
+	}
 }
 
 bool writeData_decompressed(FILE *compressed, FILE *decompressed, Binary_Tree *huffmanTree, int trash)
